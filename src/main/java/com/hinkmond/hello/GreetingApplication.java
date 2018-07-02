7ccc5b624db72ca2a23ad8ad20edd5809b489b1a
hinkmond <git@lyriad.com>
2018-07-01 17:43:48 -0700

    Created my copy of the npm package.

---
+++ src/main/java/com/hinkmond/hello/GreetingApplication.java
@@ -0,0 +1,13 @@
+package com.hinkmond.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.hinkmond.hello.GreetingApplication.class, args);
+    }
+}
