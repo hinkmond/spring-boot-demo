faf2be56566d662aa55230e4dbc426fae0b27056
hinkmond <hinkmond@gmail.com>
2018-08-25 06:22:52 +0100

    https://github.com/hinkmond/spring-boot-demo.git rk with IntelliJ IDEA and as a maven project, not gradle.

--- src/main/java/com/hinkmond/hello/GreetingApplication.java
+++ src/main/java/com/hinkmond/hello/GreetingApplication.java
@@ -2,12 +2,20 @@ package com.hinkmond.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class GreetingApplication {
+public class GreetingApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(com.hinkmond.hello.GreetingApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GreetingApplication.class);
     }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(GreetingApplication.class, args);
+    }
+
 }
